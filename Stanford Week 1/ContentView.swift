c1af1085d9778b7e89b702b4e06f5acc86d20c51
Codegenius1000 <kztonyliu@gmail.com>
2024-07-23 18:19:12 -0400

    Initial Commit

---
+++ Stanford Week 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stanford Week 1
+//
+//  Created by Tony Liu on 7/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stanford Week 1/Stanford_Week_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_Week_1App.swift
+//  Stanford Week 1
+//
+//  Created by Tony Liu on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_Week_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
